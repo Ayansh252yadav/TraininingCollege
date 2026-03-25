facb457cb71a22648b35b0fb5fccd084e9ff1bf4
Ayansh252yadav <ayanshy652@gmail.com>
2026-03-26 00:03:32 +0530

    add new file

---
+++ practice/GuessRandom.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessRandom {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Random rd=new Random();
+        int random=rd.nextInt(100);
+        System.out.println("====Enter in the era of guessing number to escape from war of loop====");
+        int count=0;
+        System.out.println("Start guessing now ......");
+        while(true){
+            int user=sc.nextInt();
+            if(user==random){
+                System.out.println(String.format("Congratulation you won this war in %s time", count));
+                break;
+            }
+            if(random>user){
+                System.out.println("Your number is less than Computer number");
+            }
+            else if(user>random){
+                System.out.println("yournumber isgreater than users number");
+            }
+            count++;
+        }
+    }
+}
